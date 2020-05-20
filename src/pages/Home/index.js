248f4084c17dd1c469f803378c859b690d53d9a0
leandro Gonçalves <leandro.gsilva@educacao.sp.gov.br>
2020-05-20 20:10:03 -0300

    include lib util to format currency

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,89 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api'
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" alt="Tenis 01"/>
-        <strong>Tenis legal</strong>
-        <span>R$29,90</span>
+export default class Home extends Component {
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  state = {
+    products: []
+  }
 
-          <span>Adicionar</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" alt="Tenis 01"/>
-        <strong>Tenis legal</strong>
-        <span>R$29,90</span>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+    const data = response.data.map(product => (
+      {
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }
+    ))
 
-          <span>Adicionar</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" alt="Tenis 01"/>
-        <strong>Tenis legal</strong>
-        <span>R$29,90</span>
+    this.setState({products: data});
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+  render () {
+    const { products } = this.state;
 
-          <span>Adicionar</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" alt="Tenis 01"/>
-        <strong>Tenis legal</strong>
-        <span>R$29,90</span>
+    return (
+      <ProductList>
+      { products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.text}/>
+            <strong>{product.text}</strong>
+            <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
 
-          <span>Adicionar</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" alt="Tenis 01"/>
-        <strong>Tenis legal</strong>
-        <span>R$29,90</span>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))
+      }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Adicionar</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ts=1586961574&ims=326x" alt="Tenis 01"/>
-        <strong>Tenis legal</strong>
-        <span>R$29,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Adicionar</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+      </ProductList>
+    );
+  }
 }
